5688e4c16df789d449c9ae38d8d1281ff7beda07
aldoseri99 <aldosariali9@gmail.com>
2024-10-15 11:18:18 +0300

    user register/login/edit/updatePassword

--- models/User.js
+++ models/User.js
@@ -1,10 +1,13 @@
-const { Schema } = require("mongoose")
+const { Schema } = require('mongoose')
 
 const userSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
     passwordDigest: { type: String, required: true },
+    phone: { type: String, required: true },
+    role: { type: String, required: true },
+    profilePic: { type: String, required: true }
   },
   { timestamps: true }
 )
--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const db = require('./db')
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 const indexRouter = require('./routes/indexRouter')
+const AuthRouter = require('./routes/AuthRouter')
 app.use('/', indexRouter)
+app.use('/auth', AuthRouter)
 
 app.use('/', (req, res) => {
   res.send(`Connected!`)
